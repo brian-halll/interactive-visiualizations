e6bb5d8308d26155c7fd35f60ddb9565bbabfbb9
brian-halll <bhall31999@gmail.com>
2022-10-05 21:49:19 -0400

    final commit for basic dashboard

--- logic3.js
+++ logic3.js
@@ -13,6 +13,7 @@ d3.json(url).then(function(data)
 {
     console.log(data);
 
+    // save metaData attributes into variables
     age = data.metadata[0].age;
     bbtype = data.metadata[0].bbtype;
     ethnicity = data.metadata[0].ethnicity;
@@ -31,7 +32,18 @@ d3.json(url).then(function(data)
     console.log(wfreq);
     */
 
-    
+    // when refering html attributes using d3 use "#" for id and "." class
+    metaDataText = d3.select(".panel-body");
+
+    // add data to metData panel body
+    metaDataText.append("p").text(`id: ${id}`);
+    metaDataText.append("p").text(`ethnicity: ${ethnicity}`);
+    metaDataText.append("p").text(`gender: ${gender}`);
+    metaDataText.append("p").text(`age: ${age}`);
+    metaDataText.append("p").text(`location: ${geo}`);
+    metaDataText.append("p").text(`bbtype: ${bbtype}`);
+    metaDataText.append("p").text(`wfreq: ${wfreq}`);
+
 
 });
 
